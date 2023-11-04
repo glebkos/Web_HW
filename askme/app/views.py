06e115f4efff1b5ab3966f47986317d91be614f8
glebkostin <glebkos777@gmail.com>
2023-11-04 09:08:47 +0300

    Сделал страницу создания вопроса Реализовал 3 пункта ДЗ1 и создал проект на Django, сформировал директорию проекта в соответсвие с ТЗ

--- askme/app/views.py
+++ askme/app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+    questions = [
+        {
+            'id': i,
+            'title': f'Question {i}'
+        } for i in range(3)
+    ]
+    return render(request, 'ask.html', {'questions': questions})
